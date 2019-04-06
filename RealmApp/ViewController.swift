889d6ce04bdf85a0d0cfbd82cba85329c7e32b72
Sahil <39743126+sahilpasha912@users.noreply.github.com>
2019-04-06 23:51:29 +0530

    Initial Commit

---
+++ RealmApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmApp
+//
+//  Created by apple on 4/6/19.
+//  Copyright © 2019 Sahil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
